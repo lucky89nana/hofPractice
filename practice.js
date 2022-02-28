9c61e310f65d6c10c7b4eeefa9aa3aff6ed82f56
lucky89nana <lilinna0809@gmail.com>
2022-02-28 15:11:40 -0800

    pass every test in Practice

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // This repo is optional extra practice to use the underscore functions.
-// Here we'll be writing new functions, but these functions will use 
-// the underscore functions within them. 
+// Here we'll be writing new functions, but these functions will use
+// the underscore functions within them.
 
 /*
  *
@@ -11,18 +11,22 @@
 // use _.each to create a copy of the given array.
 var moreFruits = function(fruits) {
   var results = [];
-
   _.each(fruits, function(fruit, index, collection) {
     results.push(fruit);
   });
-
   return results;
 };
 
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function(numbers) {
-
+  var count = 0;
+  _.each(numbers, function(number) {
+    if (number % 5 === 0) {
+      count++;
+    }
+  });
+  return count;
 };
 
 /*
@@ -34,17 +38,26 @@ var multiplesOfFive = function(numbers) {
 // use _.filter to return the fruits array with only the desired fruit.
 var onlyOneFruit = function(fruits, targetFruit) {
 
+  return _.filter(fruits, function(fruit) {
+    return fruit === targetFruit;
+  });
 };
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function(fruits, letter) {
-
+  return _.filter(fruits, function(fruit) {
+    return fruit[0] === letter;
+  });
 };
 
 // return a filtered array containing only cookie-type desserts.
+// I - object
+// O - array
 var cookiesOnly = function(desserts) {
-
+  return _.filter(desserts, function(item) {
+    return item.type === 'cookie';
+  });
 };
 
 /*
@@ -55,27 +68,47 @@ var cookiesOnly = function(desserts) {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-  
+  return _.reduce(products, function(product, item) {
+    return product + Number(item.price.slice(1));
+  }, 0);
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function(desserts) {
-
+  return _.reduce(desserts, function(obj, item) {
+    if (obj[item.type] === undefined) {
+      obj[item.type] = 1;
+      return obj;
+    } else {
+      obj[item.type]++;
+      return obj;
+    }
+  }, {});
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function(movies) {
-  
+  return _.reduce(movies, function(result, item) {
+    if (item.releaseYear >= 1990 && item.releaseYear <= 2000) {
+      result.push(item.title);
+    }
+    return result;
+  }, []);
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
 var movieNight = function(movies, timeLimit) {
-
+  return _.reduce(movies, function(bool, item) {
+    if (item.runtime < timeLimit) {
+      bool = true;
+    }
+    return bool;
+  }, false);
 };
 
 /*
@@ -87,14 +120,24 @@ var movieNight = function(movies, timeLimit) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function(fruits) {
-
+  return _.map(fruits, function(item) {
+    return item.toUpperCase();
+  });
 };
 
 // given an array of dessert objects, return a new array of objects
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function(desserts) {
-
+  return _.map(desserts, function(item) {
+    if (item.ingredients.includes('flour')) {
+      item['glutenFree'] = false;
+      return item;
+    } else {
+      item['glutenFree'] = true;
+      return item;
+    }
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -102,7 +145,7 @@ var glutenFree = function(desserts) {
 //
 // having trouble with decimals? check out this article:
 // http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
-// 
+//
 /*
 
  example output:
@@ -111,12 +154,16 @@ var glutenFree = function(desserts) {
     {
       id: 1,
       product: 'Olive Oil',
-      price: '$12.1', 
+      price: '$12.1',
       salePrice: '$9.68'
     }
   ];
 
 */
 var applyCoupon = function(groceries, coupon) {
-
+  return _.map(groceries, function(item) {
+    var saleNum = (Number(item.price.slice(1)) * (1 - coupon)).toFixed(2);
+    item['salePrice'] = '$' + saleNum;
+    return item;
+  });
 };
